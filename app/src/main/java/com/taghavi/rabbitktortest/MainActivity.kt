d8e23e5d16227ccf43d68bf2205c5683925f04a1
EhsanTgv <EhsanTgv@gmail.com>
2021-10-20 12:11:28 +0330

    Initial commit

---
+++ app/src/main/java/com/taghavi/rabbitktortest/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.taghavi.rabbitktortest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.taghavi.rabbitktortest.ui.theme.RabbitKtorTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RabbitKtorTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RabbitKtorTestTheme {
+        Greeting("Android")
+    }
+}
